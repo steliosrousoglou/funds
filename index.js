f89ebc978535f294b629c31defa53ab1f5da9426
Stylianos Rousoglou <stylianos.rousoglou@yale.edu>
2017-02-09 11:56:01 +0000

    add methods

--- index.js
+++ index.js
@@ -2,38 +2,40 @@
 var express = require('express');
 var mysql = require('mysql');
 
-/* Database Names */
-// Award database
-const award = 'test5';
+/* Database Name */
+const db_name = 'funds';
 
-//create DB connection info
+/* Database Table Names */
+// Award table
+const tb_award = 'award_dev';
+const tb_projects = 'projects_dev';
+
+/* Create Database Connection */
 var connection = mysql.createConnection({
     host: 'localhost',
     port: '3306',
     user: 'sr692',
     password: '',
-    database: 'funds',
+    database: db_name,
 });
 
-// initialize application
 var app = express();
 
-// connect to DB, either successful or error
-// queries, connections all take err as NULL argument but taking a value if error occurs
-connection.connect(function(err){
- if(!err) {
-     console.log("Database is connected ... \n\n");
-     //addAward("AHRC", "GRant2", "sdfasfccgdfg", 98765, "2016-05-05", "2020-05-05", 176543.76, "AS", "Aug-Oct 2018");
-     //createTable();  
- } else {
-     console.log("Error connecting database ... \n\n");  
- }
+connection.connect(function(err) {
+    if(!err) {
+        console.log("Connected to database.");addAward("AHRC", "GRant2", "sdfasfccgdfg", 98765, "2016-05-05", "2020-05-05", 176543.76, "AS", "Aug-Oct 2018");
+    } else {
+        console.log("Error connecting to database, quitting ... \n\n");
+        process.exit(1);
+    }
  }); 
+ 
+ /* Main methods */
  
  // Adds award to award database - funding_body_ref, finance_award_no unique
  const addAward = (Body, Name, funding_body_ref, finance_award_no, start_date, end_date, amount, status, fes_due) => {
      const post = {Body, Name, funding_body_ref, finance_award_no, start_date, end_date, amount, status, fes_due};
-     const query = 'INSERT INTO ' + award + ' SET ?'
+     const query = 'INSERT INTO ' + tb_award + ' SET ?'
      
      connection.query(query, post, function(err, rows, fields) {
          if (!err)
@@ -43,14 +45,18 @@ connection.connect(function(err){
        });
  }
  
- const createTable = () => {
-     connection.query('CREATE TABLE test5 (Body VARCHAR(10) NOT NULL, Name VARCHAR(70) NOT NULL, funding_body_ref VARCHAR(20) NOT NULL UNIQUE, finance_award_no INT UNIQUE, start_date DATE, end_date DATE, amount DOUBLE(10,2), status VARCHAR(5), fes_due VARCHAR(15))', function(err, rows) {
+  // Adds row to projects database - student-project combination unique
+ const addProject = (funding_body_ref, finance_award_no, my_finance_code, Type, EPSRC_voucher, Department, award_amount, student_name, jes, start_date, end_date, industry_account, form_s, supervisor, industrial_partner) => {
+     const post = {funding_body_ref, finance_award_no, my_finance_code, Type, EPSRC_voucher, Department, award_amount, student_name, jes, start_date, end_date, industry_account, form_s, supervisor, industrial_partner};
+     const query = 'INSERT INTO ' + tb_projects + ' SET ?'
+     
+     connection.query(query, post, function(err, rows, fields) {
          if (!err)
-           console.log('Table created');
+           console.log('Added Project');
          else
            console.log(err);
        });
  }
  
-
+/* Listen */
 app.listen(3000);
