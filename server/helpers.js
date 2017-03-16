62e2f3b5b3ffc02a824f5f66e31b6eff6fea553e
Stylianos Rousoglou <stylianos.rousoglou@yale.edu>
2017-03-16 00:59:30 +0000

    module exports at the end

--- server/helpers.js
+++ server/helpers.js
@@ -2,55 +2,67 @@
  * Helper methods of app.js
  */
 
-module.exports = {
-    callUpdate: function (json) {
-        var params = {};
-        var updates = {};
-        var split = false;
-        for (var key in json) {
-            if (json[key] == null || json[key] == '') continue;
-            else if (split) {
-                updates[key] = json[key];
-            } else if (key == "split"){
-                split = true;
-            } else {
-                params[key] = json[key];
-            }
+const callUpdate = json => {
+    var params = {};
+    var updates = {};
+    var split = false;
+    for (var key in json) {
+        if (json[key] == null || json[key] == '') continue;
+        else if (split) {
+            updates[key] = json[key];
+        } else if (key == "split"){
+            split = true;
+        } else {
+            params[key] = json[key];
         }
-        return [updates, params];
-    },
-    
-    /*
-     * Takes in JSON object, returns a formatted string of
-     * AND statements of all key-value pairs (for SQL search use)
-     * @param {json} post
-     * @return {string} st 
-     */
-    formatANDs: function(obj) {
-        var st = '';
-        for(var x in obj)
-            st += '\`' + x + (isNaN(obj[x]) ? '\`= \'' + obj[x] + '\' AND ' : '\`= ' + obj[x] + ' AND ');
-        return st.slice(0, -4);
-    },
+    }
+    return [updates, params];
+};
 
-    /*
-     * Takes in JSON object, returns a formatted string of 
-     * AND - LIKE statements of all key-value pairs (for SQL pattern matching)
-     * @param {json} post
-     * @return {string} st 
-     */
-    formatLIKEs: function(post) {
-        var st = '';
-        for(var x in post)
-            st += '\`' + x + (isNaN(post[x]) ? '\`LIKE \'%' + post[x] + '%\' AND ' : '\`= ' + post[x] + ' AND ');
-        return st.slice(0, -4);
-    },
-    
-    totalFunding: function(rows) {
-        var dict = {};
-        for (var i = 0; i < rows.length; i++) {
-            dict[rows[i].Body] = (dict[rows[i].Body] || 0) + rows[i].amount;
-        }
-        return dict;
-    },
+/*
+ * Takes in JSON object, returns a formatted string of
+ * AND statements of all key-value pairs (for SQL search use)
+ * @param {json} post
+ * @return {string} st 
+ */
+const formatANDs = obj => {
+    var st = '';
+    for(var x in obj)
+        st += '\`' + x + (isNaN(obj[x]) ? '\`= \'' + obj[x] + '\' AND ' : '\`= ' + obj[x] + ' AND ');
+    return st.slice(0, -4);
+};
+
+/*
+ * Takes in JSON object, returns a formatted string of 
+ * AND - LIKE statements of all key-value pairs (for SQL pattern matching)
+ * @param {json} post
+ * @return {string} st 
+ */
+const formatLIKEs = post => {
+    var st = '';
+    for(var x in post)
+        st += '\`' + x + (isNaN(post[x]) ? '\`LIKE \'%' + post[x] + '%\' AND ' : '\`= ' + post[x] + ' AND ');
+    return st.slice(0, -4);
+};
+
+/*
+ *  Returns json object with total funding
+ *  amount from each funding body
+ *  @param {json} rows
+ *  @return {json} dict
+ */
+
+const totalFunding = rows => {
+    var dict = {};
+    for (var i = 0; i < rows.length; i++) {
+        dict[rows[i].Body] = (dict[rows[i].Body] || 0) + rows[i].amount;
+    }
+    return dict;
+};
+
+module.exports = {
+    callUpdate,
+    formatANDs,
+    formatLIKEs,
+    totalFunding,
 };
